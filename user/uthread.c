80618b03af977a121e4dd0b27be66a5c94b7f4e6
名称 <>
2025-11-18 09:53:26 +0800

    增加线程上下文结构体并实现线程切换功能

--- user/uthread.c
+++ user/uthread.c
@@ -10,16 +10,35 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+ struct tcontext {
+
+  uint64 ra;      // 返回地址
+  uint64 sp;      // 栈指针
+
+  // callee-saved registers (被调用者保存的寄存器)
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};             
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct tcontext context;  
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
-              
+
 void 
 thread_init(void)
 {
@@ -63,6 +82,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+     thread_switch((uint64)&t->context, (uint64)&current_thread->context);
   } else
     next_thread = 0;
 }
@@ -77,6 +97,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->context.ra = (uint64)func;                   // 设置返回地址为线程函数
+  t->context.sp = (uint64)t->stack + STACK_SIZE;  // 设置栈指针指向栈顶
 }
 
 void 
